ad27d44a0759fb861014a6e73080dab4fbfca374
San Shein <santhutashein1999@gmail.com>
2022-12-06 19:10:42 +0630

    created .gitignore and add and commited all files

---
+++ src/Demo.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.Scanner;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter message type");
+		
+		
+		int check = 0;
+		do
+		{
+			String message = sc.nextLine();
+			check = 0;
+			switch(message)
+			{
+			case "A":
+				System.out.println(Priority.HIGH);
+				break;
+			case "B":
+				System.out.println(Priority.MEDIUM);
+				break;
+			case "C":
+			case "D":
+				System.out.println(Priority.LOW);
+				break;
+			default:
+				check = 1;
+				System.out.println("Enter the message again");
+			}
+		} while (check == 1);
+	}
+
+}
