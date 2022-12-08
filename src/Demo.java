0296d18c346ecf5de58470b465fd584c3c88e7ea
San Shein <santhutashein1999@gmail.com>
2022-12-08 11:08:38 +0630

    Spaces are added to Demo file

--- src/Demo.java
+++ src/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter message type");
-		
+		                     
 		
 		int check = 0;
 		do
